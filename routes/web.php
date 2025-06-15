58e7f592a2e93e41eba3d6a71603bb957fdb5391
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2025-06-15 21:42:12 +0545

    Added routes for contact

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
+use App\Models\Contact;
+Route::get('/contact', [PortfolioController::class, 'contact']);
+Route::post('/contact', [PortfolioController::class, 'contactstore']);
 
 Route::get('/', function () {
     return view('welcome');
