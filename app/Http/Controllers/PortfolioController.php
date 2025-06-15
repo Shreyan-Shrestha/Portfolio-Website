9cc253baa5dd9579cd7f2115e0a53950b4b5594d
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2025-06-15 21:19:23 +0545

    Added PortfolioController

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactRequest;
+use App\Models\Contact; 
+
+class PortfolioController extends Controller
+{
+    public function contact(){
+        return view('contact');
+    }
+}
