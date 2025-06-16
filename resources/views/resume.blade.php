e44359399078cfd91754611242f1343d947931b6
Dipakherald <88834268+Dipakherald@users.noreply.github.com>
2025-06-16 10:18:24 +0545

    Started on view/resumebladephp file

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+</head>
+<body>
+    <div class="info">
+        <label class="name">Name</label>
+        <label class="email">Email</label>
+        <label class="phone">Phone</label>
+        <img src="" alt="">
+    </div>
+
+    <div class=skill>
+        <label class="skills">Skills</label>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit non maxime a eveniet commodi, ex, ea quisquam nam cum tenetur accusantium beatae adipisci rem incidunt libero quidem quam aperiam.</p>
+    </div>
+    
+    <div class=h>
+        <label class="hobbies">Hobbies</label>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, alias laboriosam corporis odit deleniti earum illo facilis sed pariatur ab vel voluptatibus provident accusamus rem voluptatum iste, itaque veniam vero.</p>
+    </div>
+    <div class=p>
+        <label for="projects">Projects</label>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quidem sequi consequuntur architecto ad molestias, recusandae expedita aut id sint praesentium adipisci autem delectus voluptates iusto reprehenderit fugiat ratione nostrum!</p>
+    </div>
+</body>
+</html>
