24f8552a83a1bea594020424b201f69245f0080d
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2025-06-17 17:37:43 +0545

    Added navbar to partials.layout

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -8,6 +8,19 @@
 </head>
 <body>
     <main class="container">
+        <header class="header">
+            <nav class="navbar">
+            <a href="#" class="logo">Portfolio</a>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/resume">Resume</a></li>
+            </ul>
+            </nav>
+        </header>
+        
         @yield('content')
     </main>
 </body>
