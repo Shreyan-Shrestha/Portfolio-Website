3fbb9f4ce7198523cafe863811a4fd9988debdd6
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2025-06-17 09:15:12 +0545

    set routes for about

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
-use App\Models\Contact;
-Route::get('/contact', [PortfolioController::class, 'contact']);
-Route::post('/contact', [PortfolioController::class, 'contactstore']);
+
+Route::get('/about', [PortfolioController::class, 'viewabout']);
+Route::post('/about', [PortfolioController::class, 'aboutstore']);
 
 Route::get('/', function () {
     return view('welcome');
